9512c1649383168bc8f4d66d0383b28d199ca71a
Abigail <ashbella333@gmail.com>
2022-09-29 16:19:19 +0000

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1 class="bg-red-500">helooooooooooooooooooooooo</h1>
+    <h2>baaaaabaaaaaaaaaaaaababbbbbbbnaaaaaaaaaaaaabaaaaaaaa</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
