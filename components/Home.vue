883b331ff36c11ab972570b108e4a3a4ed9c89ee
Abigail <ashbella333@gmail.com>
2022-10-10 17:12:42 +0000

    added section 6

--- components/Home.vue
+++ components/Home.vue
@@ -21,29 +21,23 @@
 
     <!---------------section 2-->
 
-    <div class="w-4/5 m-auto">
-      <div class="flex gap-5">
-        <div>
-          <div class="w-96 h-full">
-            <img
-              src="images/image19.jpg "
-              class="w-full h-full pt-20 object-cover"
-            />
-          </div>
-        </div>
+    <div class="w-4/5 mx-auto flex gap-5">
+      <div class="w-96">
+        <img
+          src="images/image19.jpg "
+          class="w-full h-full pt-20 object-cover"
+        />
+      </div>
 
-        <div>
-          <div class="w-96 h-full bg-white">
-            <img src="images/image1.jpg " class="w-full h-full object-cover" />
-          </div>
-        </div>
+      <div class="w-96">
+        <img src="images/image1.jpg " class="w-full h-full object-cover" />
+      </div>
 
-        <div class="w-96 h-full bg-white">
-          <img
-            src="images/image17.jpg "
-            class="w-full h-full pt-20 object-cover"
-          />
-        </div>
+      <div class="w-96 h-full">
+        <img
+          src="images/image17.jpg "
+          class="w-full h-full pt-20 object-cover"
+        />
       </div>
     </div>
 
@@ -80,11 +74,12 @@
     <div class="w-full h-full bg-stone-600 flex justify-evenly mt-16">
       <div class="px-16">
         <img
-          src="images/image1.jpg" class="w-full h-full rounded-2xl object-cover py-20 "
+          src="images/image1.jpg"
+          class="rounded w-full h-full object-cover py-20"
         />
       </div>
       <div class="flex flex-col justify-center items-center px-16">
-        <!-- <h1 class="text-white text-7xl font-extrabold font-serif">INTERIOR</h1> -->
+        <h1 class="text-white text-7xl font-extrabold font-serif">INTERIOR</h1>
         <h1 class="text-white text-7xl font-extrabold font-serif">DESIGNS</h1>
         <p class="text-white pt-8">Engage More With Reality</p>
         <button
@@ -101,46 +96,186 @@
           Explore
         </button>
         <h2 class="mb-8 text-white text-l">Follow US</h2>
+        <div class="flex gap-5">
+          <div>
+            <svg
+              class="w-6 h-6 text-white fill-current cursor-pointer"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.162-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.099.12.112.225.085.345-.09.375-.293 1.199-.334 1.363-.053.225-.172.271-.401.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.354-.629-2.758-1.379l-.749 2.848c-.269 1.045-1.004 2.352-1.498 3.146 1.123.345 2.306.535 3.55.535 6.607 0 11.985-5.365 11.985-11.987C23.97 5.39 18.592.026 11.985.026L12.017 0z"
+              />
+            </svg>
+          </div>
+          <div>
+            <svg
+              class="w-6 h-6 text-white fill-current cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+              />
+            </svg>
+          </div>
+          <div>
+            <svg
+              class="w-6 h-6 text-white fill-current cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
 
     <!-------------section 4-->
 
-    <div class="bg-gray-100 w-full h-full m-auto">
+    <div class="bg-gray-100 w-full min-h-screen m-auto">
       <div>
-        <h1 class="text-center font-extrabold text-4xl py-20">OUR SERVICES</h1>
+        <h1 class="text-center font-semibold text-4xl pt-20 font-serif">OUR SERVICES</h1>
+        <p class="flex justify-center mt-8 mb-16"> Clear explanations of core points, detailed concept, identification
+            of sources and good examples. </p>
       </div>
 
       <div class="flex justify-between w-4/5 m-auto">
-        <div>
-          <h1 class=" font-extrabold text-2xl font-mono "  >FUNITURES</h1>
+<div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+      <img class="rounded-t-lg" src="images/image23.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-semibold mb-2 font-mono">INTERIOR DESIGNS</h5>
+      <p class="text-gray-700 text-base my-8">
+        Clear explanations of core points, detailed concept, identification
+            of sources and good examples.
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 rounded-full border-2 border-orange-600 font-medium text-xs  shadow-md hover:bg-black- hover:shadow-lg focus:shadow-lg focus:outline-none  active:shadow-lg transition duration-150 ease-in-out">Request</button>
+    </div>
+  </div>
+</div>
+
+
+<div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+      <img class="rounded-t-lg" src="images/image20.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl mb-2 font-mono font-semibold">SPACING LOGICS</h5>
+      <p class="text-gray-700 text-base my-8">
+         Clear explanations of core points, detailed concept, identification
+            of sources and good examples.
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 border-2 border-green-700 rounded-full   font-medium text-xs leading-tight hover:shadow focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out">Request</button>
+    </div>
+  </div>
+</div>
+
+
+<div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+      <img class="rounded-t-lg" src="images/image14.jpg" alt=""/>
+    
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-semibold mb-2 font-mono">ART AND DESIGN</h5>
+      <p class="text-gray-700 text-base my-8">
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 border-2 border-blue-600 rounded-full   font-medium text-xs leading-tight shadow-md hover:bg-black-700 hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out">Request</button>
+    </div>
+  </div>
+</div>
+
+        
+
+        
+        <!-- <div>
+          <h1 class="font-extrabold text-2xl font-mono">FUNITURES</h1>
           <p class="w-4/5">
             Clear explanations of core points, detailed concept, identification
             of sources and good examples.
           </p>
-        </div>
-
-        <div>
-          <h1 class=" font-extrabold text-2xl font-mono ">FUNITURES</h1>
-          <p class="w-4/5">
-            Clear explanations of core points, detailed concept, identification
-            of sources and good examples.
-          </p>
-        </div>
-        <div>
-          <h1 class=" font-extrabold text-2xl font-mono ">FUNITURES</h1>
-          <p class="w-4/5">
-            Clear explanations of core points, detailed concept, identification
-            of sources and good examples.
-          </p>
-        </div>
-      </div>
-
-
-      <div> 
-        <div><img src="images/image3.jpg " class="w-full h-full pt-20 object-cover"/> </div>
+        </div> -->
       </div>
     </div>
+
+    <!-----start of---------section 6-->
+    <section>
+      <h1 class="font-extrabold text-l text-white py-4 px-4 bg-black w-1/4 font-mono text-center">
+        RECENT WORKS
+      </h1>
+      <!-- <div class="grid grid-cols-2 grid-rows-2 gap-5 mx-auto w-4/5">
+        <div class="">
+          <div class="">
+            <img src="images/image12.jpg " class=" object-cover" />
+          </div>
+
+          <div>
+            <h3 class="">KITCHEN INTERIOR</h3>
+            <p>
+              Clear explanations of core points, detailed concept,
+              identification of sources and good examples.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div class="">
+            <img
+              src="images/image12.jpg "
+              class=" object-cover"
+            />
+          </div>
+
+          <div>
+            <h3 class="">WASHROOM INTERIOR</h3>
+            <p>
+              Clear explanations of core points, detailed concept,
+              identification of sources and good examples.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div class="">
+            <img
+              src="images/image12.jpg "
+              class=" object-cover"
+            />
+          </div>
+
+          <div>
+            <h3 class="">LIVING INTERIOR</h3>
+            <p>
+              Clear explanations of core points, detailed concept,
+              identification of sources and good examples.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div class="">
+            <img
+              src="images/image12.jpg "
+              class="object-cover"
+            />
+          </div>
+
+          <div>
+            <h3 class="">BEDROOM INTERIOR</h3>
+            <p>
+              Clear explanations of core points, detailed concept,
+              identification of sources and good examples.
+            </p>
+          </div>
+        </div>
+      </div> -->
+    </section>
   </div>
 </template>
 
