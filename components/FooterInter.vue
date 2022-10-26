dbaa6919d81a969688dfae6d27cb4d9ec46192fc
Abigail <ashbella333@gmail.com>
2022-10-26 15:56:05 +0000

    new styling for footer component

--- components/FooterInter.vue
+++ components/FooterInter.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <!------------footer-->
-    <div class="bg-black w-full h-24 ml-8">
+    <div class="bg-black w-full h-32">
+        
+      <div class="pt-8">
+        <span class="text-white text-5xl py-8 ml-4">ASHBELLA</span>
+      </div>
+
       <div>
-        <div>
-          <span class="text-white text-5xl py-8 ml-4">ASHBELLA</span>
-        </div>
-        <span>
-          <span class="text-white first-letter:">&copy; 2022 | </span>
-          <span class="text-orange-600 font-semibold"
-            >COZY INTERIOR DECOR
-          </span>
-        </span>
+        <span class="text-white first-letter:">&copy; 2022 | </span>
+        <span class="text-orange-600 font-semibold">COZY INTERIOR DECOR </span>
       </div>
     </div>
   </div>
